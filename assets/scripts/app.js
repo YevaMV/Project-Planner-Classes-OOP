2d31d5a66e082d35dc4d8ee598b0919e63b0b04f
YevaMV <eva280988@gmail.com>
2022-07-31 00:03:00 +0400

    created Project Lists

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+class Tooltip {
+
+}
+
+class ProjectItem {
+	
+}
+
+class ProjectList {
+	constructor(type) {
+		const prjItems = document.querySelectorAll(`#${type}-projects li`);
+		console.log(prjItems);
+	}
+}
+
+class App {
+	static init() {
+		const activeProjectsList = new ProjectList('active');
+		const finishedProjectsList = new ProjectList('finished')
+	}
+}
+
+App.init();
